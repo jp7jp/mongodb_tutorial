7598946d5b44138b5eb4938af2db1876420af5f3
JP Barbosa <jp@jp7.com.br>
2017-02-24 17:36:16 -0300

    CRUD Tests

--- users/src/user.js
+++ users/src/user.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
+
 const UserSchema = new Schema({
-  name: String
+  name: {
+    type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.'
+    },
+    required: [true, 'Name is required.']
+  },
+  postCount: Number
 });
 
 const User = mongoose.model('user', UserSchema);
